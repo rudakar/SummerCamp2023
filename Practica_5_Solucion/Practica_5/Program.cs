bef9688849057b9bbcc016f05bbdec23474cb502
pmpcurso1 <rubenberuete.1999@gmail.com>
2023-07-18 12:43:19 +0200

    commit inicial

---
+++ Practica_5_Solucion/Practica_5/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Practica_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lista =new List<Vehiculo>();
+             var coche_1 = new Vehiculo(1990,1000,Vehiculo.Etiqueta.sin);
+             var coche_2 = new Vehiculo(2010,1000,Vehiculo.Etiqueta.B);
+             var coche_3 = new Vehiculo(2000,1000,Vehiculo.Etiqueta.C);
+             var coche_4 = new Vehiculo(2021,1000,Vehiculo.Etiqueta.ECO);
+             var coche_5 = new Vehiculo(1970,1000,Vehiculo.Etiqueta.CERO);
+             
+            lista.Add(coche_1);
+            lista.Add(coche_2); 
+            lista.Add(coche_3);
+            lista.Add(coche_4);
+            lista.Add(coche_5);
+
+            foreach(Vehiculo coche in lista) {
+
+                double precio_final=Vehiculo.SacarPrecio(coche);
+                Console.WriteLine(precio_final);
+            
+            
+            }
+
+        }
+    }
+}
---
+++ Practica_5_Solucion/Practica_5/Vehiculo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Practica_5
+{
+    internal class Vehiculo
+    {
+        
+        public Vehiculo() { }
+        public Vehiculo(int año, int valor, Etiqueta etiqueta) {
+            Año = año;
+            ValorBase = valor;
+            Etiqueta1 = etiqueta;
+        }
+        public int  ValorBase { get; set; }
+        public int Año { get; set; }
+        public int Valor { get; set; }
+        public Etiqueta Etiqueta1  { get; set; }
+
+       
+
+        public enum Etiqueta { 
+        sin,
+        B,
+        C,
+        ECO,
+        CERO
+
+        }
+        internal static double SacarPrecio(Vehiculo coche)
+        {
+            var año_Actual = DateTime.Now.Year;
+            var difAños = año_Actual - coche.Año;
+            double porcentaje=0;
+            switch (coche.Etiqueta1) {
+                case Etiqueta.sin:
+                    porcentaje = 25;
+                    break;
+                case Etiqueta.B:
+                    porcentaje = 15;
+                    break;
+                case Etiqueta.C:
+                    porcentaje = 10;
+                    break;
+                case Etiqueta.ECO:
+                    porcentaje = 5;
+                    break;
+                case Etiqueta.CERO:
+                    porcentaje = 0;
+                    break;
+            }
+            //porcentaje = porcentaje + difAños;
+            double preciofinal = coche.ValorBase * ((porcentaje+difAños)/ 100 ) + coche.ValorBase;
+            return preciofinal;
+            
+        }
+
+    }
+
+}
+ 
