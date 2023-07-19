eda4dd04f288a46104a73479fb1fd18f093f1b20
rudakar <rubenberuete.1999@gmail.com>
2023-07-19 10:48:50 +0200

    modulo6_01

---
+++ Practica6_01_Solución/Practica6_01/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Practica6_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   var lista =new List<Vehiculo>();
+            while (true) { 
+
+                Console.WriteLine("Elige entre 1) para introducir año y color 2) para marca y modelo." +
+                    "\n Y 3) para año color marca y modelo!");
+                var input=Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        Console.WriteLine("Introduce Año: ");
+                        var año=Console.ReadLine();
+                        int año_int = Int32.Parse(año);
+                        Console.WriteLine("Introduce Color: ");
+                        var color=Console.ReadLine();
+                        var coche = new Vehiculo(año_int, color);
+                        lista.Add(coche);
+                        break;
+                    case "2":
+                        Console.WriteLine("Introduce Marca: ");
+                        var marca=Console.ReadLine();
+                        Console.WriteLine("Introduce Modelo: ");
+                        var modelo=Console.ReadLine();
+                        var coche_1 = new Vehiculo(marca,modelo);
+                        lista.Add(coche_1);
+                        break;
+                    case "3":
+                        Console.WriteLine("Introduce Año: ");
+                        var año_1 = Console.ReadLine();
+                        int año_int_1 = Int32.Parse(año_1);
+                        Console.WriteLine("Introduce Color: ");
+                        var color_1 = Console.ReadLine();
+                        Console.WriteLine("Introduce Marca: ");
+                        var marca_1 = Console.ReadLine();
+                        Console.WriteLine("Introduce Modelo: ");
+                        var modelo_1 = Console.ReadLine();
+                        var coche_3 = new Vehiculo(año_int_1,color_1,marca_1,modelo_1);
+                        lista.Add(coche_3);
+                        break;
+                    default: Console.WriteLine("Número introducido erroneo");
+                        break;
+
+
+
+
+                }
+                Console.WriteLine("Lista de coches registrados" +
+                    "\n===========================================");
+                int i = 1;
+                foreach (var coche in lista) {
+                    coche.MostrarDatos(coche, i);
+                    i++;
+                
+                }
+            }
+        }
+    }
+}
---
+++ Practica6_01_Solución/Practica6_01/Vehiculo.cs
@@ -0,0 +1,42 @@
+﻿
+namespace Practica6_01
+{
+    internal class Vehiculo
+    {
+        public int Año { get; set; }
+        public string? Color  { get; set; }
+        public string? Marca { get; set; }
+        public string? Modelo { get; set; } 
+
+        public Vehiculo() { }
+        public Vehiculo(int año,string color,string marca,string modelo)
+        {
+            Año = año;
+            Color = color;
+            Marca = marca;
+            Modelo = modelo;
+            
+        }
+        public Vehiculo(int año, string color) {
+            Año = año;
+            Color = color;
+            Marca = "_";
+            Modelo = "_";
+        }
+        public Vehiculo(string marca,string modelo)
+        {
+            Año = 00;
+            Color = "_";
+            Marca = marca;
+            Modelo = modelo;
+        }
+        public void MostrarDatos(Vehiculo v,int pos) { 
+        
+            Console.WriteLine("Coche numero:"+pos +
+                "\n Año: "+v.Año +"Color: "+ v.Color+"Marca: "+v.Marca +"Modelo: "+ v.Modelo);
+        
+        }
+        
+
+    }
+}
